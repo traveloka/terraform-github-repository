ed31318f13cc4ea5ea4803c77ca5a9c515005135
Salvian Reynaldi <salvian@traveloka.com>
2018-03-15 14:09:04 +0700

    add content

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # terraform-github-repository
+A Terraform module which creates a github repository
+
+## Usage
+```hcl
+module "my-repository" {
+    source = "github.com/traveloka/terraform-github-repository?ref=master"
+    name = "flight-api"
+    description = "flight team repository that contains flight backend API module"
+}
+
+```
+
+## Conventions
+
+
+## Authors
+  - [Salvian Reynaldi](https://github.com/salvianreynaldi)
+
+## License
+
+Apache License 2.0. See LICENSE for full details.
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "name" {
+  type = "string"
+}
+
+variable "description" {
+  type = "string"
+}
+
+variable "homepage_url" {
+  default = ""
+}
+
+variable "private" {
+  default = true
+}
+
+variable "has_issues" {
+  default = true
+}
+
+variable "has_wiki" {
+  default = false
+}
+
+variable "allow_merge_commit" {
+  default = false
+}
+
+variable "allow_squash_merge" {
+  default = false
+}
+
+variable "allow_rebase_merge" {
+  default = false
+}
+
+variable "has_downloads" {
+  default = false
+}
+
+variable "auto_init" {
+  default = true
+}
+
+variable "gitignore_template" {
+  default = ""
+}
+
+variable "license_template" {
+  default = ""
+}
+
+variable "default_branch" {
+  default = "master"
+}
