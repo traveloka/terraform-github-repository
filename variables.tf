d0e2fd5a24bef4fd73da2561631f54a1fdf7c562
Salvian Reynaldi <salvian@traveloka.com>
2018-03-20 08:58:31 +0700

    add variables description

--- variables.tf
+++ variables.tf
@@ -1,57 +1,71 @@
 variable "name" {
-  type = "string"
+  type        = "string"
+  description = "the repository name"
 }
 
 variable "description" {
-  type = "string"
+  type        = "string"
+  description = "the repository description"
 }
 
 variable "homepage_url" {
-  default = ""
+  default     = ""
+  description = "the repository homepage URL, if any"
 }
 
 variable "private" {
-  default = true
+  default     = true
+  description = "set whether the repository visibility is private"
 }
 
 variable "has_issues" {
-  default = true
+  default     = true
+  description = "enable github issues feature for the repository"
 }
 
 variable "has_wiki" {
-  default = false
+  default     = false
+  description = "enable github wiki feature for the repository"
 }
 
 variable "allow_merge_commit" {
-  default = false
+  default     = false
+  description = "allow merge commit to this repository"
 }
 
 variable "allow_squash_merge" {
-  default = true
+  default     = true
+  description = "allow squash merge commit to this repository"
 }
 
 variable "allow_rebase_merge" {
-  default = false
+  default     = false
+  description = "allow rebase merge commit to this repository"
 }
 
 variable "has_downloads" {
-  default = false
+  default     = false
+  description = "enable github download feature for the repository"
 }
 
 variable "auto_init" {
-  default = true
+  default     = true
+  description = "set whether an initial commit should be made to the repository"
 }
 
 variable "gitignore_template" {
-  default = ""
+  default     = ""
+  description = "select github gitignore tempate to init the repository with; see https://github.com/github/gitignore"
 }
 
 variable "license_template" {
-  default = ""
+  default     = ""
+  description = "select github license tempate to init the repository with; see https://github.com/github/choosealicense.com/tree/gh-pages/_licenses"
 }
 
 variable "default_branch" {
-  default = ""
+  default     = ""
+  description = "the name of the default branch of the repository. Only applicable if the repository and the branch is already created"
 }
 
 variable "owner" {
