1854fe6b7b3c54ee7d0f521214243a8562396a24
Salvian Reynaldi <salvianreynaldi@gmail.com>
2020-03-22 17:43:16 +0700

    use for_each for collaborators

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,28 +1,20 @@
+provider "github" {
+  version = "2.4.1"
+  organization = "salvianreynalditest"
+}
+
 module "this" {
   source                      = "../.."
   name                        = "my-experiment-repository"
-  description                 = "my experiment repository"
+  description                 = "my-experiment-repository"
   private                     = false
 
   repository_teams_permission = {
-    "team_1" = "pull"
-    "team_1" = "push"
-    "team_1" = "admin"
-
-    # only the last setting below ("push") will be applied for "team_1"
-    "team_1" = "push"
-    "team_2" = "push"
-    "team_3" = "pull"
+    "team1"="maintain"
+    "team2"="triage"
   }
 
   repository_collaborators_permission = {
-    "user_1" = "admin"
-    "user_2" = "pull"
-    "user_3" = "admin"
-    "user_3" = "push"
-    "user_3" = "pull"
-
-    # only the last setting below ("admin") will be applied for "user_3"
-    "user_3" = "admin"
+    "salvianreynaldi" = "admin"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 output "full_name" {
-  value       = "${github_repository.main.full_name}"
+  value       = github_repository.main.full_name
   description = "A string of the form 'orgname/reponame'"
 }
 
 output "clone_url" {
-  value       = "${github_repository.main.ssh_clone_url}"
+  value       = github_repository.main.ssh_clone_url
   description = "URL that can be provided to git clone to clone the repository via SSH"
 }
+
