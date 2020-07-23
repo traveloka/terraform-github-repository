2d7b8cc58c409f0dfa807e00ad9fc16d8c483bb6
Salvian Reynaldi <salvian@traveloka.com>
2020-07-23 17:13:22 +0700

    fix issue with team setting

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,17 +1,16 @@
 provider "github" {
-  version = "2.4.1"
   organization = "salvianreynalditest"
 }
 
 module "this" {
-  source                      = "../.."
-  name                        = "my-experiment-repository"
-  description                 = "my-experiment-repository"
-  private                     = false
+  source      = "../.."
+  name        = "my-experiment-repository"
+  description = "my-experiment-repository"
+  private     = false
 
   repository_teams_permission = {
-    "team1"="maintain"
-    "team2"="triage"
+    "team1" = "maintain"
+    "team2" = "triage"
   }
 
   repository_collaborators_permission = {
